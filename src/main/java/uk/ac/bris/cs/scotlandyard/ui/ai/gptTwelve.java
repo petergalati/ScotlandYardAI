004fd9f8b858a1c578c8d54dce40aaf8ae63715e
Peter Galati <peter8galati@gmail.com>
2023-04-10 15:37:47 +0100

    everything

---
+++ src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/gptTwelve.java
@@ -0,0 +1,25 @@
+package uk.ac.bris.cs.scotlandyard.ui.ai;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+import javax.annotation.Nonnull;
+
+
+import io.atlassian.fugue.Pair;
+import uk.ac.bris.cs.scotlandyard.model.Ai;
+import uk.ac.bris.cs.scotlandyard.model.Board;
+import uk.ac.bris.cs.scotlandyard.model.Move;
+
+public class gptTwelve implements Ai {
+
+	@Nonnull @Override public String name() { return "gptTwelve"; }
+
+	@Nonnull @Override public Move pickMove(
+			@Nonnull Board board,
+			Pair<Long, TimeUnit> timeoutPair) {
+		// returns a random move, replace with your own implementation
+		var moves = board.getAvailableMoves().asList();
+		return moves.get(new Random().nextInt(moves.size()));
+	}
+}
